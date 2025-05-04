930707633fc011ce75f5aed45aa772ce59ca2fef
AmirMahdi-for <amirmahdifor@gmail.com>
2025-05-05 00:03:29 +0330

    feat: add new migrations

---
+++ database/migrations/2025_05_04_193333_create_trades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buy_order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('sell_order_id')->constrained('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('price_per_gram');
+            $table->decimal('amount', 10, 3);
+            $table->unsignedBigInteger('total_price');
+            $table->unsignedBigInteger('commission');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trades');
+    }
+};
---
+++ database/migrations/2025_05_04_193342_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['buy', 'sell']);
+            $table->unsignedBigInteger('price_per_gram');
+            $table->decimal('amount', 10, 3);
+            $table->decimal('remaining_amount', 10, 3);
+            $table->enum('status', ['open', 'completed', 'cancelled'])->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
