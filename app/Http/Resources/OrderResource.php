da5516ee2fcfff1f8df2ceba4e00ae6b20ef1918
AmirMahdi-for <amirmahdifor@gmail.com>
2025-05-05 15:19:34 +0330

    feat: add order resource

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id"               => $this->id,
+            "user"             => new UserResource($this->user),
+            "type"             => $this->title,
+            "price_per_gram"   => $this->price_per_gram,
+            "amount"           => $this->amount,
+            "remaining_amount" => $this->remaining_amount,
+            "status"           => $this->status,
+            "created_at"       => $this->created_at,
+        ];
+    }
+}
